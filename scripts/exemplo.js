720b15b78c442d4ef71960cee2c7811196bcca1c
Henrique <henriqueeliashespanhol@gmail.com>
2025-08-18 21:27:45 -0300

    atualização  js

---
+++ scripts/exemplo.js
@@ -0,0 +1,35 @@
+// const element = document.getElementById("exemplo-get");
+        // element.style.color = 'blue';
+
+        // const novaH1 = document.createElement('h1');
+        // novaH1.textContent = "NOVO ELEMENTO H1 NA TELA";
+        // console.log("Novah1 ===>", novaH1);
+
+        // element.appendChild(novaH1);
+
+        // const header = document.getElementById("cabeça");
+        // header.style.backgroundColor = '#000';
+
+        // const footer = document.getElementById("letrao");
+        // footer.style.fontSize = '10px';
+
+        const buttonExemploJS = document.getElementById("buttonExemploJS");
+        buttonExemploJS.addEventListener('click', () => {
+            prompt("digite um numero");
+        });
+
+
+        const form = document.querySelector("form");
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            console.log('batatata')
+        });
+
+        const header = document.getElementById("cabeça");
+        header.addEventListener('mouseover', () => {
+            // alert("nao podi");
+        });
+
+        document.addEventListener('keydown', (event) => {
+            console.log(`Tecla clicada ${event.key}`);
+        });
